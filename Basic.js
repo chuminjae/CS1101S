1eddf0ed7748f3dbcfe15babd51ef1fba4475c9b
c_vgvrthrg <choochootrain1130@gmail.com>
2024-08-19 15:33:41 +0800

    update

---
+++ Basic.js
@@ -0,0 +1,17 @@
+// Basic
+// naming abstraction
+const a = 5;
+const b = 2;
+a / b; // 2.5
+a % b; // 1
+x < 1 ? a + 1: a + 5;
+// predicate ? consequent statement: alternate consequent statement
+// Only evaluate the one that is selected.
+// But the statement itself should have right syntax
+// function abstraction
+function c(/* parameter*/){
+    // argument
+}
+c(); // function call;
+// compounds are functions that I made
+// funcitons are functions imported or origninally made, primitive
